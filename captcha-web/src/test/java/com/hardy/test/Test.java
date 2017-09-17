d66dfd3ff3537e7d980478b714e89b972c75da76
lili <lilybj2009@sina.com>
2017-09-17 22:44:07 +0800

    test commit 4

--- captcha-web/src/test/java/com/hardy/test/Test.java
+++ captcha-web/src/test/java/com/hardy/test/Test.java
@@ -8,7 +8,7 @@ public class Test {
     public static void main(String[] args) throws Exception {
         System.out.print("--------- start -----------");
 
-
+        System.out.print("---------- do something ---------");
 
         System.out.print("--------- end -----------");
     }
