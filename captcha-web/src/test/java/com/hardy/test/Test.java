32ff4514bedd8b21fd35addcc06929aa07f3e799
lili <lilybj2009@sina.com>
2017-09-17 21:35:05 +0800

    test commit

---
+++ captcha-web/src/test/java/com/hardy/test/Test.java
@@ -0,0 +1,11 @@
+package com.hardy.test;
+
+/**
+ * Created by lili_ on 2017/9/17.
+ */
+public class Test {
+
+    public void test() throws Exception {
+        System.out.print("--------- OK -----------");
+    }
+}
